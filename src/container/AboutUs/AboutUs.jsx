253ae11b94d697129d2ca5cf2ded5e2f5ce82e56
neropass <drakonobor@gmail.com>
2023-01-07 21:05:15 +0300

    Created menu and fixed aboutus

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>About us</h1>
         <img src={images.spoon} alt="about spoon" className='spoon__img'/>
         <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type='button' className='custom__button'>Know more</button>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
 
       <div className='app__aboutus-content_knife flex__center'>
@@ -25,7 +25,7 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>Our History</h1>
         <img src={images.spoon} alt="about spoon" className='spoon__img'/>
         <p className='p__opensans'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
-        <button type='button' className='custom__button'>Know more</button>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
     </div>
   </div>
