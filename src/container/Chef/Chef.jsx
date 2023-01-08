9caf461d42d1a58384629e78ce488f94cc4307d0
neropass <drakonobor@gmail.com>
2023-01-08 21:47:32 +0300

    Started Chef container

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word"/>
+    </div>
   </div>
 );
 
